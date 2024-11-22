a0b696c8f18ac829c57d2e7683d5d0b77a343cf6
Chitraksh Suri <chitrakshsuri2005@gmail.com>
2024-11-22 22:57:40 +0530

    Footer Code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <>
+      <footer>
+        <div>Copyright © {currentYear}</div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
